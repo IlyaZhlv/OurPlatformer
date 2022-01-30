81883fd27f324291893e041094bb31d905a2dce7
Ilya Zhilov <zhilov702@gmail.com>
2022-01-30 19:08:08 +0300

    zombies count with icons

--- current/display.py
+++ current/display.py
@@ -29,12 +29,22 @@ class Task:
         zombie4_count_surface = self.font.render(str(zombie4_count), False, color)
 
         zombie1_count_rect = zombie1_count_surface.get_rect(topleft=(screen_width - 100, 30))
-        zombie2_count_rect = zombie1_count_surface.get_rect(topleft=(screen_width - 100, 90))
-        zombie3_count_rect = zombie1_count_surface.get_rect(topleft=(screen_width - 100, 150))
-        zombie4_count_rect = zombie1_count_surface.get_rect(topleft=(screen_width - 100, 210))
+        zombie2_count_rect = zombie2_count_surface.get_rect(topleft=(screen_width - 100, 90))
+        zombie3_count_rect = zombie3_count_surface.get_rect(topleft=(screen_width - 100, 150))
+        zombie4_count_rect = zombie4_count_surface.get_rect(topleft=(screen_width - 100, 210))
 
         for surface, rect in zip([zombie1_count_surface, zombie2_count_surface,
                                   zombie3_count_surface, zombie4_count_surface],
                                  [zombie1_count_rect, zombie2_count_rect,
                                   zombie3_count_rect, zombie4_count_rect]):
             self.display_surface.blit(surface, rect)
+
+        zombie1_surface = pygame.transform.scale(pygame.image.load('../map/zombies/zombie1/1.png').convert_alpha(), (20, 45))
+        zombie2_surface = pygame.transform.scale(pygame.image.load('../map/zombies/zombie2/1.png').convert_alpha(), (20, 45))
+        zombie3_surface = pygame.transform.scale(pygame.image.load('../map/zombies/zombie3/1.png').convert_alpha(), (20, 45))
+        zombie4_surface = pygame.transform.scale(pygame.image.load('../map/zombies/zombie4/1.png').convert_alpha(), (20, 45))
+
+        self.display_surface.blit(zombie1_surface, zombie1_surface.get_rect(topleft=(screen_width - 140, 25)))
+        self.display_surface.blit(zombie2_surface, zombie2_surface.get_rect(topleft=(screen_width - 140, 85)))
+        self.display_surface.blit(zombie3_surface, zombie3_surface.get_rect(topleft=(screen_width - 140, 145)))
+        self.display_surface.blit(zombie4_surface, zombie4_surface.get_rect(topleft=(screen_width - 140, 205)))
