098f420c33b5a9f20574abcccd6af30e74171acc
Ilya Zhilov <zhilov702@gmail.com>
2022-01-30 15:10:20 +0300

    player optimization

--- current/player.py
+++ current/player.py
@@ -1,10 +1,10 @@
-from pygame import *
+import pygame
 
 
-class Player(sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, pos, path_to_image):
         super().__init__()
-        self.current_image = image.load(path_to_image).convert_alpha()
+        self.current_image = pygame.image.load(path_to_image).convert_alpha()
         self.image = self.current_image
         self.rect = self.image.get_rect(topleft=pos)
 
@@ -14,13 +14,14 @@ class Player(sprite.Sprite):
 
         self.direct = 0
         self.helpanim = 0
-        self.animation = [image.load(path).convert_alpha() for path in
+        self.animation = [pygame.image.load(path).convert_alpha() for path in
                           ['../map/character/run/right.png', '../map/character/run/right2.png',
                            '../map/character/run/left.png', '../map/character/run/left2.png',
                            '../map/character/run/jump_left.png', '../map/character/run/jump_right.png',
                            '../map/character/run/r1.png', '../map/character/run/l1.png']]
 
         self.on_ground = True
+        self.direct = True
 
     def apply_gravity(self):
         self.y_direction += self.gravity
@@ -30,41 +31,41 @@ class Player(sprite.Sprite):
         self.y_direction = self.jump_speed
 
     def get_input(self):
-        keys = key.get_pressed()
+        keys = pygame.key.get_pressed()
 
-        if keys[K_SPACE] and self.on_ground:
+        if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
 
-        if keys[K_d]:
-            if not keys[K_SPACE]:
+        if keys[pygame.K_d] and not keys[pygame.K_a]:
+            if not keys[pygame.K_SPACE]:
                 self.image = self.current_image
                 self.current_image = self.animation[0]
                 self.current_image = self.animation[0 + self.helpanim // 5]
                 self.helpanim = (self.helpanim + 1) % 10
-                self.direct = 1
+                self.direct = True
 
             else:
                 self.image = self.current_image
                 self.current_image = self.animation[5]
-                self.lastkey = keys[K_d]
-                self.direct = 1
+                self.lastkey = keys[pygame.K_d]
+                self.direct = True
 
 
-        elif keys[K_a]:
-            if not keys[K_SPACE]:
+        elif keys[pygame.K_a] and not keys[pygame.K_d]:
+            if not keys[pygame.K_SPACE]:
                 self.image = self.current_image
                 self.current_image = self.animation[2]
                 self.current_image = self.animation[2 + self.helpanim // 5]
                 self.helpanim = (self.helpanim + 1) % 10
-                self.lastkey = keys[K_a]
-                self.direct = 0
+                self.lastkey = keys[pygame.K_a]
+                self.direct = False
             else:
                 self.image = self.current_image
                 self.current_image = self.animation[4]
-                self.lastkey = keys[K_a]
-                self.direct = 0
+                self.lastkey = keys[pygame.K_a]
+                self.direct = False
 
-        elif KEYUP:
+        elif pygame.KEYUP:
             if self.direct:
                 self.image = self.current_image
                 self.current_image = self.animation[6]
