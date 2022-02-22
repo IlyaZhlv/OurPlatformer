02d1c2322b1c5552f7c5214d34566d4b3efd0a37
divanhik <94916980+divanhik@users.noreply.github.com>
2022-02-22 15:32:41 +0300

    Update and rename current/settings.py to текущий/settings.py

--- current/settings.py
+++
@@ -1,5 +0,0 @@
-tile_size = 32
-vertical_tile_number = 30
-
-screen_width = 1200
-screen_height = tile_size * vertical_tile_number
---
+++ текущий/settings.py
@@ -0,0 +1,5 @@
+tile_size = 32
+vertical_tile_number = 30
+
+screen_width = 1920
+screen_height = 1088
